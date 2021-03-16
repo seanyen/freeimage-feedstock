89fc8557eea5b4759b9237915dfb787f8f302cad
Silvio Traversaro <silvio@traversaro.it>
2021-03-16 10:25:51 +0100

    Update FindWebP.cmake

--- recipe/cmake/FindWebP.cmake
+++ recipe/cmake/FindWebP.cmake
@@ -1,26 +1,26 @@
-# - Find WEBP
-# Find the WEBP library 
+# - Find WebP
+# Find the WebP library 
 # This module defines
-#  WEBP_INCLUDE_DIRS, where to find webp/decode.h
-#  WEBP_LIBRARIES, the libraries needed to use WEBP
+#  WebP_INCLUDE_DIRS, where to find webp/decode.h
+#  WebP_LIBRARIES, the libraries needed to use WEBP
 #
 
-find_path(WEBP_INCLUDE_DIRS
+find_path(WebP_INCLUDE_DIRS
     NAMES webp/decode.h
 )
-mark_as_advanced(WEBP_INCLUDE_DIRS)
+mark_as_advanced(WebP_INCLUDE_DIRS)
 
-find_library(WEBP_LIBRARY_RELEASE NAMES webp PATH_SUFFIXES lib)
-find_library(WEBP_LIBRARY_DEBUG NAMES webpd PATH_SUFFIXES lib)
+find_library(WebP_LIBRARY_RELEASE NAMES webp PATH_SUFFIXES lib)
+find_library(WebP_LIBRARY_DEBUG NAMES webpd PATH_SUFFIXES lib)
 
-find_library(WEBPMUX_LIBRARY_RELEASE NAMES webpmux PATH_SUFFIXES lib)
-find_library(WEBPMUX_LIBRARY_DEBUG NAMES webpmuxd PATH_SUFFIXES lib)
+find_library(WebPMUX_LIBRARY_RELEASE NAMES webpmux PATH_SUFFIXES lib)
+find_library(WebPMUX_LIBRARY_DEBUG NAMES webpmuxd PATH_SUFFIXES lib)
 
 include(SelectLibraryConfigurations)
-select_library_configurations(WEBP)
-select_library_configurations(WEBPMUX)
+select_library_configurations(WebP)
+select_library_configurations(WebPMUX)
 
-set(WEBP_LIBRARIES ${WEBPMUX_LIBRARY} ${WEBP_LIBRARY})
+set(WebP_LIBRARIES ${WebPMUX_LIBRARY} ${WebP_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(WEBP DEFAULT_MSG WEBP_INCLUDE_DIRS WEBP_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(WebP DEFAULT_MSG WebP_INCLUDE_DIRS WebP_LIBRARIES)
